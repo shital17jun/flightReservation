572040cdaeea1709479811e32f027fff7efa6c08
shital17jun <shital17jun@gmail.com>
2018-12-06 16:05:46 +0530

    //commeted testing

---
+++ amazoneappiumgit/src/amazoneappiumgit/testing.java
@@ -0,0 +1,13 @@
+package amazoneappiumgit;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//sdffsdfffdfsddsdfs
+		//sdfsfdsfsdfs
+		
+		System.out.println("safaaffasfasfas");
+	}
+
+}
